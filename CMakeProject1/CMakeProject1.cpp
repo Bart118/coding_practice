455185dfe01516c54b769185d461073a46edb8c7
Bart118 <sbart8291@gmail.com>
2024-07-30 12:30:20 -0500

    created first c++ file
    created using cmake in visual studio 2022.  Currently just prints a couple lines to the screen and waits for a user input.

---
+++ CMakeProject1/CMakeProject1.cpp
@@ -0,0 +1,15 @@
+﻿// CMakeProject1.cpp : Defines the entry point for the application.
+//
+
+#include "CMakeProject1.h"
+
+using namespace std;
+
+int main()
+{
+	char exit_ans;
+	cout << "Hello CMake." << endl;
+	cout << "Exit?" << endl;
+	cin >> exit_ans;
+	return 0;
+}
