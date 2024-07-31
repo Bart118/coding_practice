1a244fcde4f4b4ccd5090eba665e2c2ecff2e47e
Bart118 <sbart8291@gmail.com>
2024-07-31 12:24:27 -0500

    script creates a text file

--- test_script.py
+++ test_script.py
@@ -3,6 +3,15 @@
 
 import os
 
+print("creating a text file to pass parameters to executable")
+text_file = open("script_data.txt", "w")
+text_file.write("This is my text file\n")
+my_data = input("Write the second line ")
+text_file.write(my_data)
+text_file.write("\n")
+text_file.write("This is the third line\n")
+text_file.close()
+
 try:
     print("Running the program")
     os.system("C:/Users/sbart/Documents/coding_practice/CMakeProject1/out/build/x64-debug/CMakeProject1.exe")
