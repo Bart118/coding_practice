2054ed8a0fdcdd1a53d87b6f3be612d3f101c5a4
Bart118 <165840978+Bart118@users.noreply.github.com>
2024-08-07 18:55:40 -0500

    added current files with an upload

--- test_script.py
+++ test_script.py
@@ -2,14 +2,17 @@
 #last updated on: 7/31/24
 
 import os
+import random
 
+#first attempt, use less than 50 lines of data
 print("creating a text file to pass parameters to executable")
 text_file = open("script_data.txt", "w")
-text_file.write("This is my text file\n")
-my_data = input("Write the second line ")
-text_file.write(my_data)
-text_file.write("\n")
-text_file.write("This is the third line\n")
+data_count = 10  #lines of data to put in text file
+number_data = random.randint(1,1000)
+for i in range(data_count):
+    text_file.write(str(number_data))
+    text_file.write("\n")
+    number_data = random.randint(1,1000)
 text_file.close()
 
 try:
@@ -19,4 +22,23 @@ try:
 except:
     print("Something went wrong")
 
+
+#2nd attempt, use more than 50 lines
+print("creating a text file to pass parameters to executable")
+text_file = open("script_data.txt", "w")
+data_count = 60
+number_data = random.randint(1,1000)
+for i in range(data_count):
+    text_file.write(str(number_data))
+    text_file.write("\n")
+    number_data = random.randint(1,1000)
+text_file.close()
+
+try:
+    print("Running the program")
+    os.system("C:/Users/sbart/Documents/coding_practice/CMakeProject1/out/build/x64-debug/CMakeProject1.exe")
+    print("Program is finished")
+except:
+    print("Something went wrong")
+    
 input("\n\nPress enter to close.")
