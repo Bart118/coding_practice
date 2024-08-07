403d7a58acd89094b46556ac966f2ea69232816b
Bart118 <165840978+Bart118@users.noreply.github.com>
2024-08-07 18:46:25 -0500

    added updates for last few days
    added in some new stuff for the text file and converting strings to ints

---
+++ coding_practice/test_script.py
@@ -0,0 +1,44 @@
+#python script to run an executable written in c++
+#last updated on: 7/31/24
+
+import os
+import random
+
+#first attempt, use less than 50 lines of data
+print("creating a text file to pass parameters to executable")
+text_file = open("script_data.txt", "w")
+data_count = 10  #lines of data to put in text file
+number_data = random.randint(1,1000)
+for i in range(data_count):
+    text_file.write(str(number_data))
+    text_file.write("\n")
+    number_data = random.randint(1,1000)
+text_file.close()
+
+try:
+    print("Running the program")
+    os.system("C:/Users/sbart/Documents/coding_practice/CMakeProject1/out/build/x64-debug/CMakeProject1.exe")
+    print("Program is finished")
+except:
+    print("Something went wrong")
+
+
+#2nd attempt, use more than 50 lines
+print("creating a text file to pass parameters to executable")
+text_file = open("script_data.txt", "w")
+data_count = 60
+number_data = random.randint(1,1000)
+for i in range(data_count):
+    text_file.write(str(number_data))
+    text_file.write("\n")
+    number_data = random.randint(1,1000)
+text_file.close()
+
+try:
+    print("Running the program")
+    os.system("C:/Users/sbart/Documents/coding_practice/CMakeProject1/out/build/x64-debug/CMakeProject1.exe")
+    print("Program is finished")
+except:
+    print("Something went wrong")
+    
+input("\n\nPress enter to close.")
